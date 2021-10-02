ab91ae8e67f539e0292d3989e2b4f7ee7136ffa9
Alexey Boyko <alexeybbb@gmail.com>
2021-10-02 08:13:19 +0300

    init

---
+++ Services/MouseService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System;
+
+namespace BlazorDraggableDemo.Services {
+
+    public interface IMouseService {
+        event EventHandler<MouseEventArgs>? OnMove;
+        event EventHandler<MouseEventArgs>? OnUp;
+    }
+
+    public class MouseService : IMouseService {
+        public event EventHandler<MouseEventArgs>? OnMove;
+        public event EventHandler<MouseEventArgs>? OnUp;
+
+        public void FireMove(object obj, MouseEventArgs evt) => OnMove?.Invoke(obj, evt);
+        public void FireUp(object obj, MouseEventArgs evt) => OnUp?.Invoke(obj, evt);
+    }
+}
